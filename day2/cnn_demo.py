ffac57a8db09ce1bac8841a645c4154b5980f64e
XiaoRi <470840649@qq.com>
2021-10-27 17:35:10 +0800

    添加CNN

--- day2/cnn_demo.py
+++ day2/cnn_demo.py
@@ -1,7 +1,6 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 
-
 # 定义命令行参数
 tf.app.flags.DEFINE_integer('is_train', 1, "指定是否是训练模型，还是拿数据去预测")
 # 简化变量名
@@ -28,12 +27,12 @@ def create_model(x):
         conv1_weights = create_weights(shape=[5, 5, 1, 32])
         conv1_bias = create_weights(shape=[32])
 
-        conv1_x = tf.nn.conv2d(input=input_x, filter=conv1_weights, strides=[1, 1, 1, 1], padding="SAME")+ conv1_bias
+        conv1_x = tf.nn.conv2d(input=input_x, filter=conv1_weights, strides=[1, 1, 1, 1], padding="SAME") + conv1_bias
         # 输出 conv1_x = [batch_size,14,14,32] 最后一个32是通道
         # 激活函数
         relu1_x = tf.nn.relu(conv1_x)
         # 池化层
-        pool1_x = tf.nn.max_pool(value=relu1_x, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME")
+        pool1_x = tf.nn.max_pool(value=relu1_x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
         # pool1_x = [batch_size,14,14,32]
     # 第二个卷积层
     with tf.variable_scope("conv2"):
@@ -65,41 +64,72 @@ def cnn_mnist():
     # 1.准备数据
     mnist = input_data.read_data_sets("../mnist_data", one_hot=True)
     # 图片的数据，32*32 像素单通道 784 None先不确定 表示图片的张数
-    x = tf.placeholder(dtype=tf.float32, shape=[None, 784])
-    y_true = tf.placeholder(dtype=tf.float32, shape=[None, 10])
+    with tf.variable_scope("mnist_data"):
+        x = tf.placeholder(tf.float32, [None, 784])
+        y_true = tf.placeholder(tf.int32, [None, 10])
     y_predict = create_model(x)
-    # 3.构造损失函数 分类任务，这里用交叉信息熵
-    error = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_predict))
+    # 3、softmax回归以及交叉熵损失计算
+    with tf.variable_scope("softmax_crossentropy"):
+        # labels:真实值 [None, 10]  one_hot
+        # logits:全脸层的输出[None,10]
+        # 返回每个样本的损失组成的列表
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_predict))
 
-    # 4. 优化损失
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(error)
+    # 4、梯度下降损失优化
+    with tf.variable_scope("optimizer"):
+        # 学习率
+        train_op = tf.train.AdamOptimizer(0.001).minimize(loss)  # 优化器
+        # 5、得出每次训练的准确率（通过真实值和预测值进行位置比较，每个样本都比较）
+    with tf.variable_scope("accuracy"):
+        equal_list = tf.equal(tf.argmax(y_true, 1), tf.argmax(y_predict, 1))
+        accuracy = tf.reduce_mean(tf.cast(equal_list, tf.float32))
 
-    # 5. 计算准确率
-    bool_list = tf.equal(tf.argmax(y_true, axis=1), tf.argmax(y_predict, axis=1))
-    accuracy = tf.reduce_mean(tf.cast(bool_list, tf.float32))
+    # （2）收集要显示的变量
+    # 先收集损失和准确率
+    tf.summary.scalar("losses", loss)
+    tf.summary.scalar("acc", accuracy)
 
-    # 创建saver 对象
+    # 初始化变量op
+    init_op = tf.global_variables_initializer()
+
+    # （3）合并所有变量op
+    merged = tf.summary.merge_all()
+
+    # 创建模型保存和加载
     saver = tf.train.Saver()
-    # 初始化变量
-    init = tf.global_variables_initializer()
 
-    init_loss = float('inf')
-
-    # 开启会话
+    # 开启会话去训练
     with tf.Session() as sess:
-        sess.run(init)
-        if FLAGS.is_train == 1:
-            image, label = mnist.train.next_batch(100)
-            print("训练之前损失为%f" % sess.run(error, feed_dict={x: image, y_true: label}))
+        # 初始化变量
+        sess.run(init_op)
 
-            # 开始训练
+        # （1）创建一个events文件实例
+        file_writer = tf.summary.FileWriter("./tmp/summary/", graph=sess.graph)
+
+        # 加载模型
+        # if os.path.exists("./tmp/modelckpt/checkpoint"):
+        #     saver.restore(sess, "./tmp/modelckpt/fc_nn_model")
+
+        if FLAGS.is_train == 1:
+            # 循环步数去训练
             for i in range(100):
-                _, loss = sess.run([optimizer, error], feed_dict={x: image, y_true: label})
-                if loss < init_loss:
-                    init_loss = loss
-                    saver.save(sess, '../tmp/model/mnist.ckpt')
-                if (i + 1) % 10 == 0:
-                    print("第%d次训练,损失为%f" % (i + 1, loss))
+                # 获取数据，实时提供
+                # 每步提供50个样本训练
+                mnist_x, mnist_y = mnist.train.next_batch(50)
+                # 运行训练op
+                sess.run(train_op, feed_dict={x: mnist_x, y_true: mnist_y})
+                print("训练第%d步的准确率为：%f, 损失为：%f " % (i + 1,
+                                                   sess.run(accuracy, feed_dict={x: mnist_x, y_true: mnist_y}),
+                                                   sess.run(loss, feed_dict={x: mnist_x, y_true: mnist_y})
+                                                   )
+                      )
+
+                # 运行合变量op，写入事件文件当中
+                summary = sess.run(merged, feed_dict={x: mnist_x, y_true: mnist_y})
+                file_writer.add_summary(summary, i)
+                # if i % 100 == 0:
+                #     saver.save(sess, "./tmp/modelckpt/fc_nn_model")
+
         else:
             # 如果不是训练，我们就去进行预测测试集数据
             for i in range(100):
@@ -111,6 +141,7 @@ def cnn_mnist():
                     tf.argmax(sess.run(y_predict, feed_dict={x: mnist_x, y_true: mnist_y}), 1).eval()
                 )
                       )
+
     return None
 
 
